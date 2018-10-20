09258e515e79f3a8e36d44519775f2bb0c785413
AngelUrq <angel4441916@gmail.com>
2018-10-20 14:35:19 -0400

    Iniciando proyecto

---
+++ LL1/AnalizadorSintacticoLL1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LL1
+{
+    class AnalizadorSintacticoLL1
+    {
+
+        List<Produccion> listaProducciones;
+
+        public AnalizadorSintacticoLL1()
+        {
+            IniciarProducciones();
+        }
+
+        private void FactorizarIzquierda()
+        {
+            //Parte de Jhon
+        }
+
+        private void EliminarRecursividadIzquierda()
+        {
+            //Parte de Rubén
+        }
+
+        private void CalcularPrimeros()
+        {
+            //Parte de Rodri
+        }
+
+        private void CalcularSiguientes()
+        {
+            //Parte de Diego
+        }
+
+        private void RellenarTabla()
+        {
+            //Parte de Adri y Ángel
+        }
+
+        private void IniciarProducciones()
+        {
+            listaProducciones = new List<Produccion>();
+            listaProducciones.Add(new Produccion("S", "Aa"));
+            listaProducciones.Add(new Produccion("A", "BD"));
+            listaProducciones.Add(new Produccion("B", "b"));
+            listaProducciones.Add(new Produccion("B", "€"));
+            listaProducciones.Add(new Produccion("B", "b"));
+            listaProducciones.Add(new Produccion("B", "€"));
+        }
+
+    }
+}
---
+++ LL1/Produccion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LL1
+{
+    class Produccion
+    {
+
+        private string ladoDerecho;
+        private string ladoIzquierdo;
+
+        public Produccion(string ladoIzquierdo, string ladoDerecho)
+        {
+            this.ladoIzquierdo = ladoIzquierdo;
+            this.ladoDerecho = ladoDerecho;
+        }
+
+        public string GetLadoIzquierdo()
+        {
+            return ladoIzquierdo;
+        }
+
+        public string GetLadoDerecho()
+        {
+            return ladoDerecho;
+        }
+
+    }
+}
